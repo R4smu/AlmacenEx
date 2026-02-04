8fa68c5203f99cb3368353cd575f7c715ef72d11
R4smu <rasmussenmarcoslucas@gmail.com>
2026-02-04 18:36:03 +0100

    botones done capone

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
-import Tarjeta from './components/Tarjeta'
+import ComponenteBoton from './components/ComponenteBoton'
 
 function App() {
 
   return (
     <>
-      <Tarjeta{titulo="", parrafo=""}/>
+      <ComponenteBoton texto="Login" estilo="btn-login" />
+      <ComponenteBoton texto="Registrar" estilo="btn-header" />
+      <ComponenteBoton texto="Opciones" estilo="btn-opciones" />
+      <ComponenteBoton texto="Carne" estilo="btn-carne" />
+      <ComponenteBoton texto="Pescado" estilo="btn-pescado" />
+      <ComponenteBoton texto="Verdura" estilo="btn-verdura" />
+      <ComponenteBoton texto="Panaderia" estilo="btn-panaderia" />
+      <ComponenteBoton texto="Lacteos" estilo="btn-lacteos" />
+      <ComponenteBoton texto="Congelados" estilo="btn-congelados" />
+      <ComponenteBoton texto="Fruta" estilo="btn-fruta" />
     </>
   )
 }
