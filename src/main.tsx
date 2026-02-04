30716399bfd45a89fc47a872ff4e6be42642497e
Angel <agaleaa02@iesalbarregas.es>
2026-02-04 18:27:02 +0100

    mi footer

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './componentes/Header'
+import Footer from './componentes/Footer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header></Header>
+    <Footer></Footer>
   </StrictMode>,
 )
