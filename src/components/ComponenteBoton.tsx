8d08d7c6934de43f69768c33793dd31f042c75f7
yoryi404 <jalegrem04@iesalbarregas.es>
2026-02-04 20:47:03 +0100

    Terminar el Header

--- src/components/ComponenteBoton.tsx
+++ src/components/ComponenteBoton.tsx
@@ -1,6 +1,6 @@
 import "../App.css"
 
-const ComponenteBoton = ({texto, estilo}) => {
+const ComponenteBoton = ({texto, estilo}:any) => {
   return (
     <button className={estilo}>{texto}</button>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import Logo from "../assets/Logo.png"
+import ComponenteBoton from "./ComponenteBoton"
+
+const Header = () => {
+    return (
+        <section className="p-8 flex items-center justify-between bg-amber-300">
+            <div className="flex items-center">
+                <img src={Logo} alt="Logo" className="h-20 mr-4" />
+                <h1 className="text-4xl">Todo Caduca</h1>
+            </div>
+            <div className="flex gap-4">
+                <ComponenteBoton texto="Registrar" estilo="btn-header" />
+                <ComponenteBoton texto="Login" estilo="btn-header" />
+            </div>
+        </section>
+    )
+}
+export default Header
