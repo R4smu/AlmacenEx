66f204cf0bc78743d5e92ed298c6c5f8c6bd94aa
yoryi404 <jalegrem04@iesalbarregas.es>
2026-02-06 18:11:36 +0100

    Crear todo lo necesario para que se vean los componentes nuevos en esta rama

---
+++ src/Componentes.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import "./index.css"
+
+// ==============================
+// Esto es para comprobar en el navegador que funcionen los componentes que tenemos y que se ven realmente como queremos.
+// Luego este archivo lo borraremos porque no lo vamos a necesitar, asi que cuando creeis un componente nuevo,
+// ponedlo aqui tambien para que se vea en el navegador.
+// Al final, cuando meergemos la rama components-review al main,
+// solo vamos a añadir realmente toda la carpeta de components (todos los componentes nuevos que hemos creado en esta rama).
+// ==============================
+
+function Componentes() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Componentes
