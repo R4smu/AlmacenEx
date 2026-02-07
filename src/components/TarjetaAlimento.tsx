f4163bb3b2f0b642d115ede7c8154da6b59a4e35
R4smu <rasmussenmarcoslucas@gmail.com>
2026-02-07 20:49:39 +0100

    Estilos de tailwind implementados a la tarjeta

--- src/components/TarjetaAlimento.tsx
+++ src/components/TarjetaAlimento.tsx
@@ -1,18 +1,37 @@
-import { FaRegTrashCan } from "react-icons/fa6";
 import { FiEdit } from "react-icons/fi";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { CiCalendar } from "react-icons/ci";
 
-import "../App.css"
-//import "./Etiqueta.tsx"
+//import Etiqueta.tsx
 const TarjetaAlimento = () => {
   return (
     <>
-        <div className="contenedor-tarjeta-alimento">
-            <div className="">
-                
-            </div>
-        </div>
-    </>
-  )
-}
+      <div className="flex h-[150px] w-[610px] justify-between rounded-xl shadow-[0px_2px_4px_0px_black]">
 
-export default TarjetaAlimento
+        <div className="flex flex-col items-start justify-around ml-[10px]">
+          <p>Hola</p>
+          <p>Hola</p>
+
+          <div className="flex items-center justify-evenly text-gray-500">
+            <CiCalendar />
+            <p>Hola</p>
+          </div>
+        </div>
+
+        <div className="flex m-[10px]">
+
+          <div className="flex px-[10px]">
+            <FiEdit />
+          </div>
+
+          <div className="flex px-[10px]">
+            <FaRegTrashCan />
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TarjetaAlimento;
