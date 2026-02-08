9114d5708387c76777d3215276e4ee614df97624
R4smu <rasmussenmarcoslucas@gmail.com>
2026-02-08 22:38:58 +0100

    Los botones de la tarjeta actuan como tal

--- src/components/TarjetaAlimento.tsx
+++ src/components/TarjetaAlimento.tsx
@@ -7,29 +7,29 @@ const TarjetaAlimento = ({nombre_alimento, fecha, cantidad, children} : any) => 
     <>
       <div className="flex h-[150px] w-[610px] justify-between rounded-xl shadow-[0px_2px_4px_0px_black]">
 
-        <div className="flex flex-col items-start justify-around ml-[10px]">
-          <p>{nombre_alimento}</p>
+        <div className="flex flex-col items-start justify-normal ml-4 gap-3">
+          <p className="mt-4 font-semibold">{nombre_alimento}</p>
           
           <div className="flex flex-row items-start justify-around gap-2">
             {children}
           </div>
             
-          <div className="flex items-center justify-evenly text-gray-500 gap-2">
-            <CiCalendar />
+          <div className="flex items-center justify-evenly text-gray-500 font-normal gap-2">
+            <CiCalendar size={20} />
             <p>{fecha}</p>
             <p>Cantidad: {cantidad}</p>
           </div>
         </div>
 
-        <div className="flex m-[10px]">
+        <div className="flex m-[10px] items-start gap-2">
 
-          <div className="flex px-[10px] text-gray-500">
-            <FiEdit />
-          </div>
+          <button style={{ border: 'none' }} className="flex p-2 cursor-pointer" title="Editar">
+            <FiEdit size={20} />
+          </button>
 
-          <div className="flex px-[10px] text-gray-500">
-            <FaRegTrashCan />
-          </div>
+          <button style={{ border: 'none' }} className="flex p-2 cursor-pointer" title="Borrar">
+            <FaRegTrashCan size={20} />
+          </button>
 
         </div>
       </div>
