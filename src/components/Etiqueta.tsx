0dfc6bef55790058ca3ba2a6d3c873f89f1c5412
R4smu <rasmussenmarcoslucas@gmail.com>
2026-03-08 18:48:52 +0100

    arreglos en Etiqueta

--- src/components/Etiqueta.tsx
+++ src/components/Etiqueta.tsx
@@ -1,32 +1,34 @@
 import "../App.css";
-
 import { LuMilk, LuApple, LuFish, LuCarrot } from "react-icons/lu";
 import { GiMeat } from "react-icons/gi";
 import { FaBreadSlice } from "react-icons/fa6";
 import { WiSnowflakeCold } from "react-icons/wi";
 
-const ICONOS: any = {
-  lacteos: LuMilk,
-  fruta: LuApple,
-  pescados: LuFish,
-  verdura: LuCarrot,
-  carne: GiMeat,
-  panaderia: FaBreadSlice,
+const ICONOS: Record<string, any> = {
+  lacteos:    LuMilk,
+  fruta:      LuApple,
+  pescados:   LuFish,
+  verdura:    LuCarrot,
+  carne:      GiMeat,
+  panaderia:  FaBreadSlice,
   congelados: WiSnowflakeCold,
-};
+}
 
-const Etiqueta = ({ texto, tipo }: any) => {
+// Estos tipos no llevan icono
+const ESTADOS = new Set(["caducado", "apunto", "nocaducado"])
 
-  const IconoComponente = ICONOS[tipo];
-
-  const claseEstilo = `etiqueta-${tipo}`;
+const Etiqueta = ({ texto, tipo }: { texto: string; tipo: string }) => {
+  const IconoComponente = ICONOS[tipo]
+  const claseEstilo = `etiqueta-${tipo}`
 
   return (
     <div className={claseEstilo}>
-        {IconoComponente && <IconoComponente className="etiqueta-icono" />}
-        {texto}
+      {!ESTADOS.has(tipo) && IconoComponente && (
+        <IconoComponente />
+      )}
+      {texto}
     </div>
-  );
-};
+  )
+}
 
-export default Etiqueta;
+export default Etiqueta
