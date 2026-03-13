596b30498a14d8f3281fc2514d070918994e5475
yoryi404 <jalegrem04@iesalbarregas.es>
2026-03-13 17:07:54 +0100

    Implementar etiquetas "meta" en el index.html para mejorar el SEO

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -13,9 +13,9 @@ function LandingPage() {
     <div className="w-full overflow-x-hidden bg-white dark:bg-gray-800 transition-colors">
       <Hero></Hero>
       <br />
-      <Tarjeta imagen={image1} titulo='Lorem Ipsum' texto={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'}></Tarjeta><br />
-      <Tarjeta imagen={image2} titulo='Lorem Ipsum' texto={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} invertida={true}></Tarjeta><br />
-      <Tarjeta imagen={image3} titulo='Lorem Ipsum' texto={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'}></Tarjeta><br />
+      <Tarjeta imagen={image1} titulo='Lorem Ipsum' texto={'Nuestra aplicación te ayuda a llevar un control sencillo y rápido de los alimentos que compras. Podrás registrar cada producto con su cantidad y fecha de caducidad, evitando olvidar lo que tienes en casa y manteniendo tu despensa siempre organizada.'}></Tarjeta><br />
+      <Tarjeta imagen={image2} titulo='Lorem Ipsum' texto={'Muchas veces los alimentos se estropean porque no recordamos cuándo caducan. Con esta herramienta podrás ver fácilmente qué productos están próximos a vencer, lo que te permitirá consumirlos a tiempo y reducir el desperdicio de comida.'} invertida={true}></Tarjeta><br />
+      <Tarjeta imagen={image3} titulo='Lorem Ipsum' texto={'Busca, clasifica y visualiza tus alimentos de forma clara gracias a categorías y herramientas de búsqueda. Todo está pensado para que gestionar tu comida diaria sea más cómodo, rápido y eficiente.'}></Tarjeta><br />
       <Footer></Footer>
     </div>
   )
